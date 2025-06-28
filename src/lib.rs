27f459773bd4ef7eec6d7e8723b971edb4f10d63
PM <pmesidis@gmail.com>
2025-06-28 13:40:38 +0300

    restructure directories

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,4 @@
 //! B+ Tree library crate
 
-pub mod cursor;
-pub mod error;
-pub mod iterator;
-pub mod node;
 pub mod storage;
-pub mod tree;
-
-pub use crate::tree::BPlusTree;
+pub mod bplustree;
