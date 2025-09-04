f9226a7d9f6448e412d7377fd02d2b84b43064a5
PM <pmesidis@gmail.com>
2025-09-04 23:31:38 +0300

    Benchmarks folder

---
+++ benches/bench_insert.rs
@@ -0,0 +1,25 @@
+use ::bplustree::*;
+use crate::bplustree::tree::{BPlusTree, SharedBPlusTree};
+use self::storage::{file_store::FileStore, page_store::PageStore};
+use criterion::{criterion_group, criterion_main, Criterion};
+use tempfile::TempDir;
+fn benchmark_insert(c: &mut Criterion) {
+    c.bench_function("insert 1 million keys", |b| {
+        b.iter(|| {
+            let dir = TempDir::new().unwrap();
+            let file_path = dir.path().join("bplustree.db");
+            let store = FileStore::<PageStore>::new(&file_path).expect("create storage");
+            let order = 16;
+            let tree = BPlusTree::<u64, String, FileStore<PageStore>>::new(store, order)
+                .expect("create tree");
+            let st = SharedBPlusTree::new(tree);
+
+            for i in 0..1_000_000 {
+                st.insert(i, format!("val_{}", i)).unwrap();
+            }
+        });
+    });
+}
+
+criterion_group!(benches, benchmark_insert);
+criterion_main!(benches);
